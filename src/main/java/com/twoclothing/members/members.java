d7dccc0b0ad2bcdb1688c24d10238fb52864f037
cheng-han <gn01758628@gmail.com>
2023-10-06 20:16:31 +0800

    JDBC工具包/連線測試

--- src/main/java/com/twoclothing/members/members.java
+++ src/main/java/com/twoclothing/members/members.java
@@ -1,5 +1,19 @@
 package com.twoclothing.members;
 
-public class members {
+import com.twoclothing.utils.JDBCUtils;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class members {
+    Connection conn;
+
+    {
+        try {
+            conn = JDBCUtils.getConnection();
+            System.out.println("連接成功");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
---
+++ src/main/java/com/twoclothing/utils/JDBCUtils.java
@@ -0,0 +1,58 @@
+package com.twoclothing.utils;
+
+import java.sql.*;
+
+public class JDBCUtils {
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+    private static final String url = "jdbc:mysql://localhost:3306/bjpowernode?serverTimezone=Asia/Taipei";
+    private static final String user = "root";
+    private static final String password = "root";
+
+    // 註冊驅動只需註冊一次,放在static區塊,類加載時執行一次
+    static {
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 獲取資料庫連接物件
+     * @return 資料庫連接物件
+     * @throws SQLException
+     */
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    /**
+     * 釋放資源
+     * @param conn 連接物件
+     * @param ps 操作物件
+     * @param rs 結果集物件
+     */
+    public static void close(Connection conn, Statement ps, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (ps != null) {
+            try {
+                ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
