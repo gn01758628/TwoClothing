263fcc600fdfd981274c81b4dc0eff59e6d766fc
cheng-han <gn01758268@gmail.com>
2023-10-11 13:33:06 +0800

    pom檔更新/可以寫servlet/

---
+++ src/main/java/com/twoclothing/utils/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.twoclothing.utils;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print("<h1>");
+		out.print("HelloServlet");
+		out.print("</h1>");
+		
+		
+	}
+
+}
