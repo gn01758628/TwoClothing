175362a986ec750caccec1edd0e5a551cadff415
cheng-han <gn01758628@gmail.com>
2023-10-14 00:02:13 +0800

    新增InitSessionFactoryListener/OpenSessionInRequestFilter 更正BidItemBrowsing欄位設定

---
+++ src/main/java/com/twoclothing/filter/OpenSessionInRequestFilter.java
@@ -0,0 +1,30 @@
+package com.twoclothing.filter;
+
+
+import com.twoclothing.utils.HibernateUtil;
+import org.hibernate.SessionFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+//利用Annotation來註冊()
+@WebFilter("/*")
+public class OpenSessionInRequestFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        try {
+            sessionFactory.getCurrentSession().beginTransaction();
+            System.out.println("Get Session");
+            filterChain.doFilter(servletRequest, servletResponse);
+            sessionFactory.getCurrentSession().getTransaction().commit();
+            System.out.println("Session Commit");
+        } catch (Exception e) {
+            sessionFactory.getCurrentSession().getTransaction().rollback();
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+}
