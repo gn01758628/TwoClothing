e4227448193a592cc1071fc5f0a63ea4e76c398b
cheng-han <gn01758628@gmail.com>
2023-10-14 00:55:41 +0800

    配置JDBC連線池/更新測試檔

--- src/main/java/com/twoclothing/utils/test/SessionFactoryTest.java
+++ src/main/java/com/twoclothing/utils/test/SessionFactoryTest.java
@@ -3,11 +3,12 @@ package com.twoclothing.utils.test;
 import com.twoclothing.utils.HibernateUtil;
 import org.hibernate.SessionFactory;
 
-public class Test {
+public class SessionFactoryTest {
     public static void main(String[] args) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
         SessionFactory sessionFactory1 = HibernateUtil.getSessionFactory();
         System.out.println(sessionFactory);
         System.out.println(sessionFactory1);
+        HibernateUtil.shutdown();
     }
 }
