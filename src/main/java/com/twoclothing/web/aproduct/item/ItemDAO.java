fc9e02ff2ee0fc3d9e741a3ee884002fef82c544
wenzai <ax26851@gmail.com>
2023-10-13 10:11:14 +0800

    reset

--- src/main/java/com/twoclothing/web/aproduct/item/ItemDAO.java
+++ src/main/java/com/twoclothing/web/aproduct/item/ItemDAO.java
@@ -5,18 +5,18 @@ import java.util.List;
 
 public interface ItemDAO {
 	
-	 public int insert(Item item);
+	 public void insert(Item item);
 
 	 public Item getByPrimaryKey(Integer itemId);
 
 	 public List<Item> getAll();
 
-	 public List<Item> getAllByTagId(Integer tagId);
+	 public List<Item> getAllByTadId(Integer tadId);
 
 	 public List<Item> getAllByMbrId(Integer mbrId);
 	 
 	 public List<Item> getAllByItemStatus(Integer itemStatus);
 
-	 public int update(Item item);
+	 public void update(Integer itemId);
 
 }
