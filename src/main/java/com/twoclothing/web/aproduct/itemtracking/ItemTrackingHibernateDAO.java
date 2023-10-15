78a6f002e3b54bb7524b71f04f37fcb54bf31ae6
Chi <cqiqi610@gmail.com>
2023-10-15 17:54:55 +0800

    ItemTrackingHibernateDAO yet

---
+++ src/main/java/com/twoclothing/web/aproduct/itemtracking/ItemTrackingHibernateDAO.java
@@ -0,0 +1,52 @@
+package com.twoclothing.web.aproduct.itemtracking;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class ItemTrackingHibernateDAO implements ItemTrackingDAO {
+	private SessionFactory factory;
+
+	public ItemTrackingHibernateDAO(SessionFactory factory) {
+		this.factory = factory;
+	}
+
+	public Session getSession() {
+		return factory.getCurrentSession();
+	}
+
+	@Override
+	public void insert(ItemTracking itemTracking) {
+	}
+
+	@Override
+	public ItemTracking getByCompositeKey(Integer itemId, Integer mbrId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ItemTracking> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ItemTracking> getAllByItemId(Integer itemId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ItemTracking> getAllByMbrId(Integer mbrId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Integer itemId, Integer mbrId) {
+		// TODO Auto-generated method stub
+
+	}
+}
