51005b84a52d0bd1aefce7f9942aceafc0f01ed2
wenzai <ax26851@gmail.com>
2023-10-17 13:53:23 +0800

    變更package位置

---
+++ src/main/java/com/twoclothing/huiwen/service/ItemServiceImpl.java
@@ -0,0 +1,63 @@
+package com.twoclothing.huiwen.service;
+
+import java.util.List;
+
+import com.twoclothing.model.aproduct.item.Item;
+import com.twoclothing.model.aproduct.item.ItemDAO;
+import com.twoclothing.model.aproduct.item.ItemHibernateDAO;
+import com.twoclothing.utils.HibernateUtil;
+
+
+public class ItemServiceImpl implements ItemService{
+	
+	private ItemDAO dao;
+	
+	public ItemServiceImpl() {
+		dao = new ItemHibernateDAO(HibernateUtil.getSessionFactory());
+	}
+	
+	@Override
+	public Item addItem(String itemName, String detail, Integer tagId, Integer mbrId, Integer price, Integer itemStatus, Integer quantity) {
+		
+		Item item = new Item();		
+		item.setItemName(itemName);
+		item.setDetail(detail);
+		item.setTagId(tagId);
+		item.setMbrId(mbrId);
+		item.setPrice(price);
+		item.setItemStatus(itemStatus);
+		item.setQuantity(quantity);
+		return item;
+	}
+
+	@Override
+	public Item updateItem(Item item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteItem(Integer itemId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Item getItemByItemId(Integer itemId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Item> getAllItems(int page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getPageTotal() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
