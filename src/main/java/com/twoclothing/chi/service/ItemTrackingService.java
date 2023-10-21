eea4790066a241059ee9ac74553f65b84e453c78
Chi <cqiqi610@gmail.com>
2023-10-21 10:12:02 +0800

    Add ItemTracking Function get*.add.delete

--- src/main/java/com/twoclothing/chi/service/ItemTrackingService.java
+++ src/main/java/com/twoclothing/chi/service/ItemTrackingService.java
@@ -5,9 +5,15 @@ import java.util.List;
 import com.twoclothing.model.aproduct.itemtracking.ItemTracking;
 
 public interface ItemTrackingService {
+	List<ItemTracking> getAllItemTracking(int currentPage);
+	
+	int getPageTotal();
+	
+	List<ItemTracking> getAllByMbrId(Integer mbrId, int currentPage);
+	
+	ItemTracking getByPrimaryKey(Integer itemId, Integer mbrId);
+	
 	ItemTracking addItemTracking(ItemTracking itemTracking);
 
 	int deleteItemTracking(Integer itemId, Integer mbrId);
-
-	ItemTracking getByPrimaryKey(Integer itemId, Integer mbrId);
 }
