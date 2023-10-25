e0170715d33dae5efc7925a551bdb23dec0dfc4d
wenzai <ax26851@gmail.com>
2023-10-25 12:11:09 +0800

    Item加上傳圖片功能

--- src/main/java/com/twoclothing/huiwen/controller/ItemServlet.java
+++ src/main/java/com/twoclothing/huiwen/controller/ItemServlet.java
@@ -3,8 +3,10 @@ package com.twoclothing.huiwen.controller;
 import static com.twoclothing.huiwen.Constants.ITEM_PAGE_MAX_RESULT;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
-import java.util.Enumeration;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -12,16 +14,20 @@ import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import com.twoclothing.huiwen.service.ItemService;
 import com.twoclothing.huiwen.service.ItemServiceImpl;
 import com.twoclothing.model.aproduct.item.Item;
+import com.twoclothing.model.aproduct.itemimage.ItemImage;
 
 @WebServlet("/Item/*")
+@MultipartConfig(fileSizeThreshold = 1024 * 1024, maxFileSize = 2 * 1024 * 1024, maxRequestSize = 2 * 2 * 1024 * 1024)
 public class ItemServlet extends HttpServlet {
 
 	private ItemService itemService;
@@ -40,98 +46,211 @@ public class ItemServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
 		res.setCharacterEncoding("UTF-8");
+		req.setCharacterEncoding("UTF-8");
 		res.setContentType("text; charset=UTF-8");
 		String action = req.getParameter("action");
 
 		PrintWriter out = res.getWriter();
 
-		System.out.println(action);
+		System.out.println("action:" + action);
+
 		// 新增資料
 		if ("insert".equals(action)) {
-			System.out.println("insert");
+
 			List<String> errorMsgs = new LinkedList<String>();
 			req.setAttribute("errorMsgs", errorMsgs);
 
 			String itemName = req.getParameter("itemName");
-			if (itemName == null || itemName.trim().length() == 0) {
-				errorMsgs.add("商品名稱請勿空白");
-				return;
-			}
+			if (itemName == null || itemName.trim().isEmpty())
+				errorMsgs.add("商品標題不可以空白");
+
+			String grade = req.getParameter("grade");
+			if (grade == null || grade.trim().isEmpty() || !grade.matches("^[0-5]$"))
+				errorMsgs.add("請正確選擇商品新舊程度");
+
+			String size = req.getParameter("size");
+			if (size == null || size.trim().isEmpty() || !size.matches("^[0-8]$"))
+				errorMsgs.add("請正確選擇商品品尺寸");
 
 			String detail = req.getParameter("detail");
-
-			Integer grade = null;
-			try {
-				grade = Integer.valueOf(req.getParameter("grade").trim());
-			} catch (NumberFormatException e) {
-				errorMsgs.add("新舊請填數字");
-				return;
+			if (detail == null || detail.trim().isEmpty()) {
+				errorMsgs.add("商品簡述不可以空白");
+			} else {
+				if (detail.replace("<br>", " ").length() > 250)
+					errorMsgs.add("商品簡述不可以超過250個字");
 			}
 
-			Integer size = null;
-			try {
-				size = Integer.valueOf(req.getParameter("size").trim());
-			} catch (NumberFormatException e) {
-				errorMsgs.add("尺寸請填數字");
-				return;
+			String tagId = req.getParameter("tagId");
+			System.out.println("tagId:" + tagId);
+			List<Integer> allSelectableTagsId = itemService.getAllSelectableTagsId();
+			System.out.println("列表"+allSelectableTagsId);
+			if (tagId == null || tagId.trim().isEmpty()) {
+System.out.println("類別錯1");
+				errorMsgs.add("請正確選擇商品類別標籤");
+			} 
+//			else if (!allSelectableTagsId.contains(Integer.parseInt(tagId))) {
+//System.out.println("類別錯2");
+//				errorMsgs.add("您選擇的類別標籤並非是可選標籤");
+//			}
+
+			String quantity = req.getParameter("quantity");
+			if (quantity == null) {
+				errorMsgs.add("請正確填寫數量");
+			} else {
+				if (!quantity.isEmpty()) {
+					if (!quantity.matches("[1-9][0-9]*")) {
+						errorMsgs.add("數量必須是有效的正整数，且大於0");
+					}
+				}
+			}
+			String price = req.getParameter("price");
+			if (price == null) {
+				errorMsgs.add("請正確填寫價格");
+			} else {
+				if (!price.isEmpty()) {
+					if (!price.matches("[1-9][0-9]*"))
+						errorMsgs.add("價格必須是有效的正整数，且大於0");
+				}
 			}
 
-			Integer tagId = null;
+			Part imgPart1 = null, imgPart2 = null;
 			try {
-				tagId = Integer.valueOf(req.getParameter("tagId").trim());
-			} catch (NumberFormatException e) {
-				errorMsgs.add("類別請填數字");
-				return;
+				Collection<Part> parts = req.getParts();
+				for (Part part : parts) {
+					if ("image01".equals(part.getName()))
+						imgPart1 = part;
+					if ("image02".equals(part.getName()))
+						imgPart2 = part;
+				}
+
+				// 判斷檔案類型是否錯誤
+				boolean image01TypeInvalid = imgPart1 != null && imgPart1.getSize() > 0
+						&& !imgPart1.getContentType().startsWith("image/");
+				boolean image02TypeInvalid = imgPart2 != null && imgPart2.getSize() > 0
+						&& !imgPart2.getContentType().startsWith("image/");
+				if (image01TypeInvalid || image02TypeInvalid) {
+					errorMsgs.add("圖片檔案類型錯誤(只接受圖檔)");
+				}
+				// 單張圖片超過大小的例外處理
+			} catch (IllegalStateException e) {
+				errorMsgs.add("單張圖片的大小不能超過2MB");
 			}
 
-			Integer mbrId = null;
-			try {
-				mbrId = Integer.valueOf(req.getParameter("mbrId").trim());
-			} catch (NumberFormatException e) {
-				errorMsgs.add("員編請填數字");
-				return;
+			// 取得byte[]圖片
+			byte[] image01 = null, image02 = null;
+			if (imgPart1 != null && imgPart2 != null) {
+				InputStream in = imgPart1.getInputStream();
+				image01 = in.readAllBytes();
+				in = imgPart2.getInputStream();
+				image02 = in.readAllBytes();
+				in.close();
+			}
+			if (image01 == null || image01.length == 0)
+				errorMsgs.add("每個商品都必須要有主圖片");
+			if (image02 == null)
+				errorMsgs.add("請正確的選擇商品補充圖片");
+
+			// 如果錯誤訊系不為空則轉發回新增頁面
+			if (!errorMsgs.isEmpty()) {
+				System.out.println("不為空");
+	            req.getRequestDispatcher("/front_end/item/itemSellerUpload.jsp").forward(req, res);
 			}
 
-			Integer price = null;
-			try {
-				price = Integer.valueOf(req.getParameter("price").trim());
-			} catch (NumberFormatException e) {
-				errorMsgs.add("價格請填數字");
-				return;
+//			if (!errorMsgs.isEmpty()) {
+//				req.getRequestDispatcher("/front_end/item/itemSellerUpload.jsp").forward(req, res);
+//			}
+//	        List<CategoryTags> allCategoryTags = itemService.getAllCategoryTags();
+//	        req.setAttribute("categoryTags", allCategoryTags);
+			Item item = new Item();
+			item.setItemName(itemName);
+			item.setGrade(Integer.parseInt(grade));
+			item.setDetail(detail);
+			item.setTagId(Integer.parseInt(tagId));
+			item.setPrice(Integer.parseInt(price));
+			item.setQuantity(Integer.parseInt(quantity));
+			item.setItemStatus(0);
+			Integer mbrId = 1;
+			item.setMbrId(mbrId);
+			if ("8".equals(size)) {
+				item.setSize(null);
+			} else {
+				item.setSize(Integer.parseInt(size));
 			}
 
-			Integer itemStatus = null;
-			try {
-				itemStatus = Integer.valueOf(req.getParameter("itemStatus").trim());
-			} catch (NumberFormatException e) {
-				errorMsgs.add("狀態請填數字");
-				return;
+			int itemPK = itemService.addItem(item);
+
+			ItemImage itemImage01 = new ItemImage();
+			itemImage01.setItemId(itemPK);
+			itemImage01.setImage(image01);
+			itemService.addItemImage(itemImage01);
+
+			if (image02.length != 0) {
+				ItemImage itemImage02 = new ItemImage();
+				itemImage02.setItemId(itemPK);
+				itemImage02.setImage(image02);
+				itemService.addItemImage(itemImage02);
 			}
 
-			Integer quantity = null;
-			try {
-				quantity = Integer.valueOf(req.getParameter("quantity"));
-			} catch (NumberFormatException e) {
-				errorMsgs.add("數量請填數字");
-				return;
-			}
+			String url = "/Item/searchAll?action=getAll";
+			RequestDispatcher dispatcher = req.getRequestDispatcher(url);
+			dispatcher.forward(req, res);
+		}
 
-			itemService.addItem(itemName, grade, size, detail, tagId, mbrId, price, itemStatus, quantity);
+		if ("getOne".equals(action)) {
+			System.out.println("拿到getOne");
 
+			Integer itemId = Integer.valueOf(req.getParameter("itemId"));
+
+			Item item = itemService.getItemByItemId(itemId);
+
+			req.setAttribute("item", item);
+			String url = "/front_end/item/itemSellerUpdate.jsp";
+			RequestDispatcher dispatcher = req.getRequestDispatcher(url);
+			dispatcher.forward(req, res);
+		}
+		if ("update".equals(action)) {
+//			System.out.println("拿到Update");
+//
+//			Integer itemId = Integer.valueOf(req.getParameter("itemId"));
+//			String itemName = req.getParameter("itemName");
+//			Integer price = Integer.valueOf(req.getParameter("price"));
+//			Integer size = Integer.valueOf(req.getParameter("size"));
+//			Integer grade = Integer.valueOf(req.getParameter("grade"));
+//			String detail =req.getParameter("detail");
+//			Integer quantity = Integer.valueOf(req.getParameter("quantity"));
+//			System.out.println(itemId+"/"+ itemName+"/"+grade+"/"+size+"/"+detail+"/"+price+"/"+quantity);
+//
+//			int itemUpdate = itemService.updateItem(itemId, itemName, grade, size, detail, price, quantity) ;
+//			if(itemUpdate == 1) {
+//				System.out.println("修改成功");
+//				
+//			}else {
+//				System.out.println("修改失敗");
+//
+//			}
+//			
+//			String url = "/front_end/item/itemSellerSearch.jsp";
+//
+//			RequestDispatcher dispatcher2 = req.getRequestDispatcher(url);
+//			dispatcher2.forward(req, res);
+
+		}
+
+		if ("getAll".equals(action)) {
+			getAllItemsPage(req, res);
+			RequestDispatcher dispatcher = req.getRequestDispatcher("/front_end/item/itemSellerListAll.jsp");
+			dispatcher.forward(req, res);
 		}
 
 		// 複合查詢
 		if ("search".equals(action)) {
 			String actionGetAll = req.getParameter("action");
+			System.out.println("actionGetAll:" + actionGetAll);
 			String forwardPath = "";
 //			if (str == null || (str.trim().length()) == 0) {
 //				return;
 //			}
 			switch (actionGetAll) {
-			case "getAll":
-				System.out.println("拿到getAll路徑");
-				forwardPath = getAllItemsPage(req, res);
-				break;
 			case "search":
 				System.out.println("拿到search路徑");
 				forwardPath = getCompositeItemQuery(req, res);
@@ -152,8 +271,12 @@ public class ItemServlet extends HttpServlet {
 
 	// 查全部
 	private String getAllItemsPage(HttpServletRequest req, HttpServletResponse res) {
+
 		String page = req.getParameter("page");
+		System.out.println("page+" + page);
+
 		int pageNow = (page == null) ? 1 : Integer.parseInt(page);
+		System.out.println("00001");
 
 		List<Item> itemList = itemService.getAllItems(pageNow);
 
@@ -161,13 +284,14 @@ public class ItemServlet extends HttpServlet {
 			int itemPageQty = itemService.getPageTotal();
 			req.setAttribute("itemPageQty", itemPageQty);
 		}
-
+		Collections.reverse(itemList);
 		req.setAttribute("itemList", itemList);
 		req.setAttribute("pageNow", pageNow);
 
 		return "/front_end/item/itemSellerListAll.jsp";
 	}
 
+	// 複合查詢
 	private String getCompositeItemQuery(HttpServletRequest req, HttpServletResponse res) {
 //		Map<String, String[]> map = req.getParameterMap();
 		Map<String, String[]> map = new HashMap<>(req.getParameterMap());
@@ -219,9 +343,10 @@ public class ItemServlet extends HttpServlet {
 			req.setAttribute("pageNow", pageNow);
 			req.setAttribute("itemList", itemList);
 		} else {
-			System.out.println("map.size()==0");
+			System.out.println("map.sizes()==0");
 
 		}
 		return "/front_end/item/itemSellerListCompositeQuery.jsp";
 	}
+
 }
--- src/main/webapp/front_end/item/itemSellerUpload.jsp
+++ src/main/webapp/front_end/item/itemSellerUpload.jsp
@@ -1,94 +1,3 @@
-<%-- <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%> --%>
-<%-- <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%> --%>
-
-<!-- <!DOCTYPE html> -->
-<!-- <html> -->
-<!-- <head> -->
-<!-- <meta charset="UTF-8"> -->
-<!-- <link rel="stylesheet" href=""> -->
-<!-- <title>itemSellerUpload</title> -->
-<!-- <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> --> -->
-<!-- <style> -->
-
-
-<!-- </style> -->
-
-<!-- </head> -->
-<!-- <body> -->
-
-<%-- <!-- --><c:if test="${not empty errorMsgs}"> --%>
-<!-- 	<font style="color:red">請修正以下錯誤:</font> -->
-<!-- 	<ul> -->
-<%-- 		<c:forEach var="message" items="${errorMsgs}"> --%>
-<%-- 			<li style="color:red">${message}</li> --%>
-<%-- 		</c:forEach> --%>
-<!-- 	</ul> -->
-<%-- </c:if>  --%>
-
-
-<!-- <form class="form_add" method="post" action="/TwoClothing/Item/itemUpload" enctype="multipart/form-data"> -->
-<!-- 	<h1>商品上傳</h1> -->
-<!-- 	<br> -->
-<!-- 	<label>商品名稱</label><br> -->
-<!-- 	<input type="text" name="itemName" maxlength = 20> -->
-<!-- 	<br> -->
-<!-- 	<label>商品新舊程度</label><br> -->
-<!-- 	<select  name="grade"> -->
-<!-- 		<option value="" selected>請選擇商品新舊程度</option> -->
-<!-- 		<option value="0">全新(沒使用過且包裝未拆,吊牌未剪)</option> -->
-<!-- 		<option value="1">9成5新(沒有使用痕跡,但包裝已拆,吊牌已剪)</option> -->
-<!-- 		<option value="2">9成新(有使用痕跡,但沒有瑕疵)</option> -->
-<!-- 		<option value="3">8成新(有使用痕跡,少量瑕疵)</option> -->
-<!-- 		<option value="4">5成新(有使用痕跡,明顯瑕疵)</option> -->
-<!--            <option value="5">破損商品(需要修補)</option> -->
-<!-- 	</select> -->
-<!-- 	<br> -->
-<!-- 	<label>尺寸</label><br> -->
-<!-- 	<select  name="size"> -->
-<!-- 		<option value="9" selected>如果您的商品不是以下列選項來描述尺寸，請跳過此選擇</option> -->
-<!-- 		<option value="0">XS(含)以下</option> -->
-<!-- 		<option value="1">S</option> -->
-<!-- 		<option value="2">M</option> -->
-<!-- 		<option value="3">L</option> -->
-<!-- 		<option value="4">XL</option> -->
-<!-- 		<option value="5">2XL</option> -->
-<!-- 		<option value="6">3XL</option> -->
-<!-- 		<option value="7">4XL(含)以上</option> -->
-<!-- 	</select> -->
-<!-- <!-- 	<br> --> -->
-<!-- <!-- 	<label>類別</label><br> --> -->
-<!-- 	<input type="hidden" name="tagId" value="1" display="none"> -->
-<!-- <!-- 	<br> --> -->
-<!-- <!-- 	<label>員編</label><br> --> -->
-<!-- 	<input type="hidden" name="mbrId" value="1" display="none"> -->
-<!-- 	<br> -->
-<!-- 	<label>價格</label><br> -->
-<!-- 	<input type="text" name="price"> -->
-<!-- <!-- 	<br> --> -->
-<!-- <!-- 	<label>狀態</label><br> --> -->
-<!-- 	<input type="hidden" name="itemStatus" value="0" display="none"> -->
-<!-- 	<br> -->
-<!-- 	<label>數量</label><br> -->
-<!-- 	<input type="text" name="quantity" autocomplete="on"> -->
-<!-- 	<br> -->
-<!-- 	<label>商品描述</label><br> -->
-<!-- 	<textarea name="detail"></textarea> -->
-<!-- 	<br> -->
-<!-- <!-- 	<div> --> -->
-<!-- <!-- 	<label>上傳商品主圖片</label> --> -->
-<!-- <!-- 	<input type="file" name="itemImg01" accept="image/jpeg, image/png"> --> -->
-<!-- <!-- 	</div> --> -->
-<!--     <br>	 -->
-<!-- <input type="hidden" name="action" value="insert"> -->
-<!-- <input type="submit" value="送出新增"> -->
-<!-- </form>	 -->
-
-<%--     <a href="${pageContext.request.contextPath}/front_end/item/itemSellerSearch.jsp">查詢</a> --%>
-
-<!-- <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script> --> -->
-<!-- </body> -->
-<!-- </html> -->
-
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 
@@ -102,18 +11,71 @@
 	<title>itemSellerUpload</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 	<style type="text/css">
-	form {
-		border: 1px solid black;
-	 	text-align:center; 
-	}
-	
-	label{
-		text-align : center;
-	}
-	
-	input{
-		text-align : center;
-	}
+/* 添加一些全局样式，设置字体和背景颜色 */
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f4;
+    margin: 0;
+    padding: 0;
+}
+
+/* 创建一个包含表单的容器 */
+.form-container {
+    max-width: 500px;
+    margin: 0 auto;
+    background-color: #fff;
+    padding: 20px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+}
+
+/* 设置表单元素的样式 */
+.form-container label {
+    display: block;
+    font-weight: bold;
+    margin-bottom: 5px;
+}
+
+.form-container input[type="text"],
+.form-container select,
+.form-container textarea {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+}
+
+.form-container input[type="file"] {
+    width: 100%;
+    margin-bottom: 10px;
+}
+
+.form-container select {
+    background-color: #f9f9f9;
+}
+
+.form-container textarea {
+    resize: vertical;
+}
+
+.form-container input[type="submit"] {
+    background-color: #007BFF;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+/* 添加一些响应式设计，适应不同屏幕尺寸 */
+@media (max-width: 768px) {
+    .form-container {
+        max-width: 100%;
+        padding: 10px;
+    }
+}
+
 	</style>
 </head>
 <body>
@@ -128,7 +90,7 @@
 </c:if>
 
 
-<form class="form_add" method="post" action="/TwoClothing/Item/add" >
+<form class="form_add" method="post" action="${pageContext.request.contextPath}/TwoClothing/Item/add" enctype="multipart/form-data">
 <p>
 	<h1>商品上傳</h1>
 	<br>
@@ -136,29 +98,66 @@
 	<input type="text" name="itemName" maxlength = 20>
 	<br>
 	<label>商品新舊程度</label>
-	<input type="text" name="grade">
+<!-- 	<input type="text" name="grade"> -->
+	<select  name="grade">
+		<option value="" selected>請選擇商品新舊程度</option>
+		<option value="0">全新(沒使用過且包裝未拆,吊牌未剪)</option>
+		<option value="1">9成5新(沒有使用痕跡,但包裝已拆,吊牌已剪)</option>
+		<option value="2">9成新(有使用痕跡,但沒有瑕疵)</option>
+		<option value="3">8成新(有使用痕跡,少量瑕疵)</option>
+		<option value="4">5成新(有使用痕跡,明顯瑕疵)</option>
+        <option value="5">破損商品(需要修補)</option>
+	</select>
 	<br>
 	<label>尺寸</label>
-	<input type="text" name="size">
+<!-- 	<input type="text" name="size"> -->
+	<select  name="size"> 
+		<option value="9" selected>如果您的商品不是以下列選項來描述尺寸，請跳過此選擇</option>
+		<option value="0">XS(含)以下</option>
+		<option value="1">S</option>
+		<option value="2">M</option>
+		<option value="3">L</option>
+		<option value="4">XL</option>
+		<option value="5">2XL</option>
+		<option value="6">3XL</option>
+		<option value="7">4XL(含)以上</option>
+	</select>
 	<br>
 	<label>類別</label>
-	<input type="text" name="tagId" value="1">
-	<br>
-	<label>員編</label>
-	<input type="text" name="mbrId" value="1">
+	<select  name="tagId"> 
+		<option value="1" selected>所有種類</option>
+		<option value="2">上衣</option>
+		<option value="3">褲子</option>
+		<option value="4">飾品</option>
+		<option value="5">短袖</option>
+		<option value="6">長袖</option>
+		<option value="7">短褲</option>
+		<option value="8">長褲</option>
+		<option value="9">男短袖</option>
+		<option value="10">女短袖</option>
+	</select>
 	<br>
 	<label>價格</label>
-	<input type="text" name="price">
-	<br>
-	<label>狀態</label>
-	<input type="text" name="itemStatus" value="0">
+	<input type="text" name="price" autocomplete="on">
 	<br>
 	<label>數量</label>
-	<input type="text" name="quantity" autocomplete="on">
+	<input type="number" name="quantity" value="1" min="1">
 	<br>
 	<label>商品描述</label>
 	<textarea name="detail"></textarea>
 	<br>
+    <div class="">
+      <label for="image01" class="form-label">上傳商品的主圖片<span class="text-danger">*</span></label>
+      <input class="form-control" type="file" id="image01" name="image01"
+             accept="image/jpeg, image/png" required aria-describedby="image01Help">
+      <div id="image01Help" class="form-text">每個商品都必須要有主圖片</div>
+    </div>
+
+    <div class="">
+      <label for="image02" class="form-label">上傳商品的補充圖片</label>
+      <input class="form-control" type="file" id="image02" name="image02"
+             accept="image/jpeg, image/png">
+    </div>
 		
 	
 <input type="hidden" name="action" value="insert">
@@ -167,6 +166,30 @@
 </form>	
     <a href="${pageContext.request.contextPath}/front_end/item/itemSellerSearch.jsp">查詢</a>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+<!--bootstrap5 js-->
+<script src="${pageContext.request.contextPath}/js/bootstrap5/popper.min.js"></script>
+<script src="${pageContext.request.contextPath}/js/bootstrap5/bootstrap.min.js"></script>
+<!--jQuery-->
+<script src="${pageContext.request.contextPath}/js/jQuery/jquery-3.7.1.min.js"></script>
+<!--輸入錯誤訊息的資料(必須在引用標籤js檔之前宣告)-->
+<script>
+    const messages = [];
+    <c:forEach var="errorMsgs" items="${errorMsgs}">
+    messages.push("${errorMsgs}");
+    </c:forEach>
+</script>
+<!--此頁面的js-->
+<%-- <script src="${pageContext.request.contextPath}/js/chengHan/addBid.js" type="text/javascript"></script> --%>
+<script>
+//     const categoryData = [
+//         <c:forEach var="tags" items="${categoryTags}" begin="1">
+//         {id:${tags.tagId}, name: '${tags.categoryName}', parentId:${tags.superTagId}},
+//         </c:forEach>
+//     ];
+</script>
+
+<!--商品類別標籤的js-->
+<%-- <script src="${pageContext.request.contextPath}/js/chengHan/addBidCategoryTags.js"></script> --%>
+
 </body>
 </html>
