81a33f9efe77976fea5b56a60497956ef9707bfd
tony <tonyyoyo1986@gmail.com>
2023-10-26 23:14:18 +0800

    新增競標商品檢舉的查詢(含複合查詢)，尚欠新增及修改

---
+++ src/main/java/com/twoclothing/tonyhsieh/service/BidItemReportService.java
@@ -0,0 +1,27 @@
+package com.twoclothing.tonyhsieh.service;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Map;
+
+import com.twoclothing.model.abid.biditemreport.BidItemReport;
+
+
+
+public interface BidItemReportService {
+
+	BidItemReport addBidItemReport(Integer bidItemId, Integer mbrId, Integer empId, Timestamp reportDate,
+			String bidDescription, Integer bidStatus, Timestamp auditDate, Integer result, String note);
+	
+	BidItemReport update(Integer reportId, Integer bidItemId, Integer mbrId, Integer empId, Timestamp reportDate,
+			String bidDescription, Integer bidStatus, Timestamp auditDate, Integer result, String note);
+
+   
+   List<BidItemReport> getAll();
+
+   List<BidItemReport> getBidITReportByCompositeQuery(Map<String, String[]> map);
+	
+   
+   
+	
+}
