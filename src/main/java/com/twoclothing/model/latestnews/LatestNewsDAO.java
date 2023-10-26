43d771af764e8716587b7fc5b2d9c16f5884de6f
ChiJung <aa860318tw@gmail.com>
2023-10-27 03:50:11 +0800

    更改部分DAO 方法名稱

--- src/main/java/com/twoclothing/model/latestnews/LatestNewsDAO.java
+++ src/main/java/com/twoclothing/model/latestnews/LatestNewsDAO.java
@@ -4,11 +4,11 @@ import java.util.List;
 import java.sql.Timestamp;
 
 public interface LatestNewsDAO {
-    void addLatestNews(LatestNews latestNews);
-    void updateLatestNews(LatestNews latestNews);
-    void deleteLatestNews(Integer newsId);
-    LatestNews getLatestNewsById(Integer newsId);
-    List<LatestNews> getAllLatestNews();
-    List<LatestNews> getLatestNewsByEmployeeId(Integer empId);
-    List<LatestNews> getLatestNewsByTimestamp(Timestamp timestamp); // 新增一个根据时间戳查询的方法
+    void insert(LatestNews latestNews);
+    LatestNews getByPrimaryKey(Integer newsId);
+    List<LatestNews> getAll();
+    List<LatestNews> getAllByEmpId(Integer empId);
+    List<LatestNews> getAllByTimestamp(Timestamp timestamp); // 新增一个根据时间戳查询的方法
+    void update(LatestNews latestNews);
+    void delete(Integer newsId);
 }
