f18c547ec87f9811e6a77bed13668949300378b0
ChiJung <aa860318tw@gmail.com>
2023-10-28 01:55:09 +0800

    富文本編輯器 (尚未完成版)

--- src/main/java/com/twoclothing/model/latestnews/LatestNewsDAO.java
+++ src/main/java/com/twoclothing/model/latestnews/LatestNewsDAO.java
@@ -4,11 +4,17 @@ import java.util.List;
 import java.sql.Timestamp;
 
 public interface LatestNewsDAO {
-    void insert(LatestNews latestNews);
-    LatestNews getByPrimaryKey(Integer newsId);
-    List<LatestNews> getAll();
-    List<LatestNews> getAllByEmpId(Integer empId);
-    List<LatestNews> getAllByTimestamp(Timestamp timestamp); // 新增一个根据时间戳查询的方法
-    void update(LatestNews latestNews);
-    void delete(Integer newsId);
+	void insert(LatestNews latestNews);
+
+	LatestNews getByPrimaryKey(Integer newsId);
+
+	List<LatestNews> getAll();
+
+	List<LatestNews> getAllByEmpId(Integer empId);
+
+	List<LatestNews> getAllByTimestamp(Timestamp timestamp); // 新增一个根据时间戳查询的方法
+
+	void update(LatestNews latestNews);
+
+	void delete(Integer newsId);
 }
