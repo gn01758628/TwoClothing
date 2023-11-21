e4618b0212afef943073e0e8044325a7a434cb3e
cheng-han <gn01758628@gmail.com>
2023-11-22 02:30:19 +0800

    競標商品瀏覽頁面(類別查詢未完成)

---
+++ src/main/java/com/twoclothing/chenghan/controller/BidItemBrowseServlet.java
@@ -0,0 +1,99 @@
+package com.twoclothing.chenghan.controller;
+
+import com.google.gson.Gson;
+import com.twoclothing.chenghan.dto.BidIItemDTO;
+import com.twoclothing.model.abid.biditem.BidItem;
+import com.twoclothing.model.abid.biditem.BidItemDAO;
+import com.twoclothing.model.abid.biditem.BidItemHibernateDAO;
+import com.twoclothing.redismodel.bidrecord.BidRecord;
+import com.twoclothing.redismodel.bidrecord.BidRecordDAO;
+import com.twoclothing.redismodel.bidrecord.BidRecordJedisDAO;
+import com.twoclothing.utils.FormatUtil;
+import com.twoclothing.utils.HibernateUtil;
+import org.hibernate.SessionFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet("/front/biditemBrowse")
+public class BidItemBrowseServlet extends HttpServlet {
+
+    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+    private final BidItemDAO bidItemDAO = new BidItemHibernateDAO(sessionFactory);
+
+    private final static BidRecordDAO bidRecordDAO = new BidRecordJedisDAO();
+
+    private final Gson gson = new Gson();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+
+        Integer tagId = Integer.parseInt(request.getParameter("tagId"));
+        Integer currentPage = Integer.parseInt(request.getParameter("currentPage"));
+        Integer pageMaxResult = Integer.parseInt(request.getParameter("pageMaxResult"));
+        Map<String, String> data = new HashMap<>();
+        List<BidIItemDTO> bidIItemDTOList;
+        List<BidItem> bidItemList;
+        int resultCount, quotient, totalPage;
+        if (1 == tagId) {
+            // 計算結果總數,計算總頁數
+            resultCount = bidItemDAO.countByActive();
+            quotient = resultCount / pageMaxResult;
+            totalPage = resultCount % pageMaxResult == 0 ? quotient : quotient + 1;
+            // 根據currentPage,pageMaxResult查詢資料
+            bidItemList = bidItemDAO.getAllLimit(currentPage, pageMaxResult);
+        } else {
+            // 有指定類別時
+            // 計算結果總數,計算總頁數
+            resultCount = bidItemDAO.countByTags(tagId);
+            quotient = resultCount / pageMaxResult;
+            totalPage = resultCount % pageMaxResult == 0 ? quotient : quotient + 1;
+            // 根據currentPage,pageMaxResult查詢資料
+            bidItemList = bidItemDAO.getAllByTagsLimit(currentPage, pageMaxResult, tagId);
+        }
+        bidIItemDTOList = getDTOList(bidItemList);
+        // 傳送資料
+        data.put("totalPage", String.valueOf(totalPage));
+        data.put("bidIItemDTOList", gson.toJson(bidIItemDTOList));
+        out.write(gson.toJson(data));
+    }
+
+    private static List<BidIItemDTO> getDTOList(List<BidItem> bidItemList) {
+        List<BidIItemDTO> bidIItemDTOList = new ArrayList<>();
+        if (bidItemList != null && !bidItemList.isEmpty()) {
+            for (BidItem bidItem : bidItemList) {
+                BidIItemDTO dto = new BidIItemDTO();
+                Integer bidItemId = bidItem.getBidItemId();
+                dto.setBidItemId(bidItemId);
+                dto.setEndTime(bidItem.getEndTime().getTime());
+                dto.setBidName(bidItem.getBidName());
+
+                BidRecord record = bidRecordDAO.getIndexRecordByKey(bidItemId, 0);
+                if (record != null) {
+                    Integer currentBid = FormatUtil.parseFormattedNumber(record.getBidAmount());
+                    dto.setBidAmountType("目前出價：");
+                    dto.setCurrentBid(currentBid);
+                } else {
+                    dto.setBidAmountType("起標金額：");
+                    dto.setCurrentBid(bidItem.getStartPrice());
+                }
+                bidIItemDTOList.add(dto);
+            }
+        }
+        return bidIItemDTOList;
+    }
+}
--- src/main/java/com/twoclothing/utils/test/ServletTest.java
+++ src/main/java/com/twoclothing/utils/test/ServletTest.java
@@ -1,12 +1,9 @@
 package com.twoclothing.utils.test;
 
-import com.twoclothing.redismodel.notice.Notice;
-import com.twoclothing.redismodel.notice.NoticeDAO;
-import com.twoclothing.redismodel.notice.NoticeJedisDAO;
+import com.twoclothing.model.categorytags.CategoryTags;
 import com.twoclothing.utils.HibernateUtil;
 import com.twoclothing.utils.JedisPoolUtil;
 import org.hibernate.SessionFactory;
-import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import javax.servlet.ServletException;
@@ -27,35 +24,9 @@ public class ServletTest extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // HibernateUtil Test
-        System.out.println("SessionFactory：" + sessionFactory);
-        // JedisPoolUtil Test
-        System.out.println("JedisPool：" + jedisPool);
-        Jedis jedis = jedisPool.getResource();
-        // 選擇自己的空間
-        jedis.select(15);
-        jedis.set("test", "HelloRedis");
-        System.out.println(jedis.get("test"));
-        // 關閉資源
-        jedis.close();
-
-        Notice notice = new Notice();
-        notice.setType("競標審核");
-        notice.setHead("沒通過拉~");
-        notice.setContent("這什麼鬼東西你也敢PO");
-        notice.setLink("/xxx/xxx");
-        notice.setImageLink("/ooo/ooo");
-
-        NoticeDAO noticeDAO = new NoticeJedisDAO();
-        noticeDAO.insert(notice, 1);
-        noticeDAO.insert(notice, 1);
-        noticeDAO.insert(notice, 1);
-        noticeDAO.insert(notice, 1);
-
-        List<Notice> allByMbrId = noticeDAO.getAllByMbrId(1);
-        for (Notice n : allByMbrId) {
-            System.out.println(n);
+        List<CategoryTags> list = (List<CategoryTags>) getServletContext().getAttribute("categoryTags");
+        for (CategoryTags tag : list) {
+            System.out.println(tag);
         }
-
     }
 }
